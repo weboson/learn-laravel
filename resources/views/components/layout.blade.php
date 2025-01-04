76ae6600a2f65a343c673a25fbcdfbb9727a08c5
Rishat <rishat.khalikov@yandex.ru>
2025-01-04 08:13:39 +0500

    Used: @slot, nav

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,3 +1,28 @@
-<div>
-    <h2>My Component</h2>
-</div>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>{{ env('APP_NAME') }}</title>
+</head>
+<body class="bg-slate-100 text-slate-900">
+<header class="bg-slate-800 shadow-lg">
+    <nav>
+        <a href="#" class="nav-link">Home</a>
+
+        <div class="flex items-center gap-4">
+            <a href="#" class="nav-link">Login</a>
+            <a href="#" class="nav-link">Register</a>
+        </div>
+    </nav>
+</header>
+<main class="py-8 px-4 mx-auto max-w-screen-lg">
+    {{--  $slot - это содержимое в, которое указывается при подключении этого компонента (почти как children в raect) --}}
+    {{ $slot }}
+</main>
+</body>
+</html>
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,4 @@
-{{--подключает еще один blade--}}
-@extends('layout')
-{{--секция main - которую подключается через yield('main)--}}
-@section('main')
-    <h1>Hello, World!</h1>
-
 {{--    подключение component--}}
-{{--    <x-layout/>--}}
-@endsection
+<x-layout>
+    <h1>Hello, World!</h1>
+</x-layout>
