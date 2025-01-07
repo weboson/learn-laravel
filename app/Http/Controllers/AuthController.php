6d1fc5280350df23eab215289435eec141a6598b
Rishat <rishat.khalikov@yandex.ru>
2025-01-07 14:25:46 +0500

    Learn: Validations data from Form

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,19 @@ class AuthController extends Controller
 {
     // метод для post (routes/web.php)
     public function register(Request $request) { // $request создали экземпляр класса Request
-        dd($request->username); // Чтобы посмотреть, что содержит Request. Или даже определенное поле формы (username)
+
+        //Validate
+        $request->validate([
+            'username' => ['required', 'max:255'],
+            'email' => ['required', 'max:255', 'email'],
+            'password' => ['required', 'min:3', 'confirmed'], // confirmed - должен быть подтвержден в поле password_confirmation
+        ]);
+
+
+        dd($request->username); // Что бы посмотреть, что содержит Request. Или даже определенное поле формы (username)
+//Register
+//Login
+//Redirect
     }
 
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,25 +9,39 @@
             {{-- Username--}}
             <div class="mb-4">
                 <label for="username">Username</label>
-                <input type="text" name="username" class="input">
+                <input type="text" name="username" class="input" value="{{ old('username') }}" style="@error('username') border: 1px solid red @enderror">
+            {{--сообщение об ошибке при валидации из AuthController--}}
+                @error('username')
+                <p class="error">{{ $message }}: - поле не должно быть пустым.</p>
+                @enderror
             </div>
 
             {{-- Email--}}
             <div class="mb-4">
                 <label for="email">Email</label>
-                <input type="text" name="email" class="input">
+                <input type="text" name="email" class="input" value="{{ old('email') }}" style="@error('email') border: 1px solid red @enderror">
+                @error('email')
+                <p class="error">{{ $message }}: - поле не должно быть пустым.</p>
+                @enderror
             </div>
 
             {{-- Password--}}
             <div class="mb-4">
                 <label for="password">Password</label>
-                <input type="password" name="password" class="input">
+                <input type="password" name="password" class="input" style="@error('password') border: 1px solid red @enderror">
+
+                @error('password')
+                <p class="error">{{ $message }}</p>
+                @enderror
             </div>
 
             {{-- Confirm Password--}}
             <div class="mb-8">
                 <label for="password_confirmation">Confirm Password</label>
-                <input type="password" name="password_confirmation" class="input">
+                <input type="password" name="password_confirmation" class="input" style="@error('password') border: 1px solid red @enderror">
+                @error('password')
+                <p class="error">{{ $message }}</p>
+                @enderror
             </div>
 
             {{--Submit Button--}}
